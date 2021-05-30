f760ebea4afe7d67a803c0b81106dc0d10737b11
jai-the-seeker <karma.jaimandeep@gmail.com>
2021-05-30 23:26:03 +0530

    added README

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Ideas on Scaling up security architecture for deploying web Apps",
+        "excerpt":"Let us take a use case in which a small to medium sized enterprise wants to scale up the security infrastructure of their internet facing website. They have this conventional architecture in which the website is hosted on Virtual Private Server (VPS). What can be done immediately, for upgrading the...","categories": [],
+        "tags": [],
+        "url": "/musings/web-apps/",
+        "teaser": null
+      }]
